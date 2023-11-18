2025fd1dd58e42af3a74aae994132808e350e0b1
Ben Green <ben.green@greencodemedia.co.uk>
2023-11-18 11:01:21 +0000

    FIX: not running

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    tsconfigRootDir: __dirname,
-    sourceType: "module",
-  },
   plugins: ["@typescript-eslint/eslint-plugin"],
   extends: ["standard-with-typescript", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-  env: {
-    node: true,
-  },
   ignorePatterns: ["build/**/*"],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "import/prefer-default-export": "off",
     "import/no-unresolved": "off",
